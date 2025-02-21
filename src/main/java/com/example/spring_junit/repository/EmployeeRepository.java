c964ddfe584dad7f92eb85d30ade6f3354fb0531
Saurabh-Desale01 <saurabh.desale0101@gmail.com>
2025-02-21 22:38:11 +0530

    repository added

--- src/main/java/com/example/spring_junit/repository/EmployeeRepository.java
+++ src/main/java/com/example/spring_junit/repository/EmployeeRepository.java
@@ -1,4 +1,7 @@
 package com.example.spring_junit.repository;
 
-public interface EmployeeRepository {
+import com.example.spring_junit.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
